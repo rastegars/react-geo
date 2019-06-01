3f0a9edbc27a01a475df7fcf4174119ed13f9beb
Sajad Rastegar <rastegar.sajjad@gmail.com>
2019-06-01 04:31:40 +0430

    Lint main.js

--- src/__tests__/Main.js
+++ src/__tests__/Main.js
@@ -1,8 +1,12 @@
-import React from 'react'
-import {render, cleanup, waitForElement, waitForDomChange, fireEvent} from 'react-testing-library'
-import 'jest-dom/extend-expect'
-import axiosMock from 'axios'
-import Main from '../components/Main'
+// @flow
+import React from 'react';
+import {
+  render, cleanup, waitForElement, waitForDomChange, fireEvent,
+} from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import axiosMock, { $AxiosError } from 'axios';
+import type { $AxiosXHR } from 'axios';
+import Main from '../components/Main';
 
 afterEach(cleanup);
 jest.mock('axios');
@@ -13,98 +17,82 @@ let response = {
       id: 1,
       location: 'Place Name',
       lat: 48.85,
-      lon: 2.35
-    }
-  ]
-}
+      lon: 2.35,
+    },
+  ],
+};
 
 test('displays google map', () => {
-  axiosMock.get.mockResolvedValueOnce(response)
+  axiosMock.get.mockResolvedValueOnce(response);
 
-  const {getByTestId, getAllByText} = render(<Main />)
+  const { getByTestId } = render(<Main />);
 
-  expect(getByTestId('google-map-container')).toBeInTheDocument()
+  expect(getByTestId('google-map-container')).toBeInTheDocument();
 });
 
 describe('Component loads', () => {
   describe('successful request', () => {
     test('fetches saved places', async () => {
-      axiosMock.get.mockResolvedValueOnce(response)
+      axiosMock.get.mockResolvedValueOnce(response);
 
-      const {getByTestId, getAllByText} = render(<Main />)
+      const { getAllByText } = render(<Main />);
 
-      await waitForElement(() =>
-        getAllByText('Place Name'),
-      )
+      await waitForElement((): HTMLElement => getAllByText('Place Name'));
     });
 
     test('displays markers on the map', async () => {
-      axiosMock.get.mockResolvedValueOnce(response)
+      axiosMock.get.mockResolvedValueOnce(response);
 
-      const {getByTestId} = render(<Main />)
+      const { getByTestId } = render(<Main />);
 
-      await waitForElement(() =>
-        getByTestId('marker'),
-      )
-    })
-  })
+      await waitForElement((): HTMLElement => getByTestId('marker'));
+    });
+  });
 
   describe('failled request', () => {
     test('displays error message', async () => {
-      axiosMock.get.mockImplementation((url) => {
-        return Promise.reject();
-      });
+      axiosMock.get.mockImplementation((): Promise<$AxiosXHR<$AxiosError>> => Promise.reject());
 
-      const {getByText} = render(<Main />)
+      const { getByText } = render(<Main />);
 
-      await waitForElement(() =>
-        getByText('Something Went Wrong!')
-      )
+      await waitForElement((): HTMLElement => getByText('Something Went Wrong!'));
     });
-  }) 
-})
+  });
+});
 
 describe('remove button', () => {
   describe('on success', () => {
     test('removes the place from the list', async () => {
-      axiosMock.get.mockResolvedValueOnce(response)
-      axiosMock.delete.mockResolvedValueOnce(response)
+      axiosMock.get.mockResolvedValueOnce(response);
+      axiosMock.delete.mockResolvedValueOnce(response);
 
-      const {getByText, queryByText} = render(<Main />)
+      const { getByText, queryByText } = render(<Main />);
 
-      await waitForElement(() =>
-        getByText('Remove'),
-      )
+      await waitForElement((): HTMLElement => getByText('Remove'));
 
-      fireEvent.click(getByText('Remove'))
+      fireEvent.click(getByText('Remove'));
 
-      await waitForDomChange()
+      await waitForDomChange();
 
-      expect(queryByText('Place Name')).toBeNull()
+      expect(queryByText('Place Name')).toBeNull();
     });
   });
 
   describe('on error', () => {
     test('displays error message', async () => {
-      axiosMock.get.mockResolvedValueOnce(response)
-      axiosMock.delete.mockImplementation((url) => {
-        return Promise.reject();
-      });
+      axiosMock.get.mockResolvedValueOnce(response);
+      axiosMock.delete.mockImplementation((): Promise<$AxiosXHR<$AxiosError>> => Promise.reject());
 
-      const {getByText, queryByText} = render(<Main />)
+      const { getByText } = render(<Main />);
 
-      await waitForElement(() =>
-        getByText('Remove'),
-      )
+      await waitForElement((): HTMLElement => getByText('Remove'));
 
-      fireEvent.click(getByText('Remove'))
+      fireEvent.click(getByText('Remove'));
 
-      await waitForElement(() =>
-        getByText('Something Went Wrong!')
-      )
+      await waitForElement((): HTMLElement => getByText('Something Went Wrong!'));
     });
   });
-})
+});
 
 describe('when the save button is clicked in search result', () => {
   const searchResult = {
@@ -113,21 +101,22 @@ describe('when the save button is clicked in search result', () => {
         place_id: 1,
         display_name: 'Place Name',
         lat: '48.85',
-        lon: '2.35'
-      }
-    }
-  ]}
+        lon: '2.35',
+      },
+    },
+    ],
+  };
 
-  const places =  {
+  const places = {
     data: [
       {
         id: 1,
         location: 'Place Name',
         lat: 48.85,
-        lon: 2.35
-      }
-    ]
-  }
+        lon: 2.35,
+      },
+    ],
+  };
 
   describe('on successful save', () => {
     test('adds location to dom', async () => {
@@ -135,76 +124,68 @@ describe('when the save button is clicked in search result', () => {
         data: {
           id: 2,
           location: 'New Place',
-          lat: "-4.479925",
-          lon: "-63.5185396"
-        }
-      }
+          lat: '-4.479925',
+          lon: '-63.5185396',
+        },
+      };
 
-      axiosMock.get.mockImplementation((url) => {
+      axiosMock.get.mockImplementation((url: string): Promise<$AxiosXHR> => {
         if (new URL(url).pathname === '/places/search') {
           return Promise.resolve(searchResult);
-        } else {
-          return Promise.resolve(places);
         }
+        return Promise.resolve(places);
       });
 
-      axiosMock.post.mockResolvedValueOnce(response)
+      axiosMock.post.mockResolvedValueOnce(response);
 
-      const {getAllByText, getByTestId, getByPlaceholderText, getByText} = render(
-        <Main />
-      )
+      const {
+        getByTestId, getByPlaceholderText, getByText,
+      } = render(
+        <Main />,
+      );
 
-      fireEvent.change(getByPlaceholderText("Search a location"), { target: { value: 'paris' }})
+      fireEvent.change(getByPlaceholderText('Search a location'), { target: { value: 'paris' } });
 
-      await waitForElement(() =>
-        getByTestId('search-result'),
-      )
+      await waitForElement((): HTMLElement => getByTestId('search-result'));
 
-      expect(getByTestId('search-result')).toHaveTextContent('Place Name')
+      expect(getByTestId('search-result')).toHaveTextContent('Place Name');
 
-      fireEvent.click(getByText(/save/i))
+      fireEvent.click(getByText(/save/i));
 
-      await waitForElement(() =>
-        getByTestId('saved-places'),
-      )
+      await waitForElement((): HTMLElement => getByTestId('saved-places'));
 
-      expect(getByTestId('saved-places')).toHaveTextContent('New Place')
-    })
-  })
+      expect(getByTestId('saved-places')).toHaveTextContent('New Place');
+    });
+  });
 
   describe('on save failed', () => {
     test('displays error message', async () => {
-      axiosMock.get.mockImplementation((url) => {
+      axiosMock.get.mockImplementation((url: string): Promise<$AxiosXHR> => {
         if (new URL(url).pathname === '/places/search') {
           return Promise.resolve(searchResult);
-        } else {
-          return Promise.resolve(places);
         }
+        return Promise.resolve(places);
       });
 
-      axiosMock.post.mockImplementation((url) => {
-        return Promise.reject();
-      });
+      axiosMock.post.mockImplementation((): Promise<$AxiosXHR<$AxiosError>> => Promise.reject());
 
-      const {getAllByText, getByTestId, getByPlaceholderText, getByText} = render(
-        <Main />
-      )
+      const {
+        getByTestId, getByPlaceholderText, getByText,
+      } = render(
+        <Main />,
+      );
 
-      fireEvent.change(getByPlaceholderText("Search a location"), { target: { value: 'paris' }})
+      fireEvent.change(getByPlaceholderText('Search a location'), { target: { value: 'paris' } });
 
-      await waitForElement(() =>
-        getByTestId('search-result'),
-      )
+      await waitForElement((): HTMLElement => getByTestId('search-result'));
 
-      expect(getByTestId('search-result')).toHaveTextContent('Place Name')
+      expect(getByTestId('search-result')).toHaveTextContent('Place Name');
 
-      fireEvent.click(getByText(/save/i))
+      fireEvent.click(getByText(/save/i));
 
-      await waitForElement(() =>
-        getByText('Something Went Wrong!')
-      )
+      await waitForElement((): HTMLElement => getByText('Something Went Wrong!'));
 
-      expect(getByText('Something Went Wrong!')).toBeInTheDocument()
-    })
-  })
-})
+      expect(getByText('Something Went Wrong!')).toBeInTheDocument();
+    });
+  });
+});
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,96 +1,121 @@
 // @flow
-import React, { PureComponent } from 'react'
-import axios from 'axios'
-import GoogleMap from './GoogleMap'
-import Search from './Search'
-import '../styles/Main.css'
+import React, { PureComponent } from 'react';
+import axios from 'axios';
+import GoogleMap from './GoogleMap';
+import Search from './Search';
+import '../styles/Main.css';
 
-type Location = {
+type LocationType = {
   id: number,
   location: string,
   lat: string,
   lon: string
 };
 
-type Locations = Array<Location>;
+type LocationsType = Array<LocationType>;
 
-type Props = {};
+type PropsType = {};
 
-type State = {
-  locations: Locations,
+type StateType = {
+  locations: LocationsType,
   error: boolean,
   fullScreen: boolean
 };
 
-class Main extends PureComponent<Props, State> {
-  constructor(props: Props) {
-    super(props)
-    this.state = { locations: [], error: false, fullScreen: false }
+class Main extends PureComponent<PropsType, StateType> {
+  constructor(props: PropsType) {
+    super(props);
+    this.state = { locations: [], error: false, fullScreen: false };
   }
 
   componentDidMount() {
-    this.getPlaces()
-    window.addEventListener("fullscreenchange", this._toggleFullScreen);
+    this.getPlaces();
+    window.addEventListener('fullscreenchange', this.toggleFullScreen);
   }
 
-  _toggleFullScreen = () =>
-    this.setState(prevState => ({fullScreen: !prevState.fullScreen}))
+  toggleFullScreen = () => {
+    this.setState((prevState: StateType): {fullScreen: boolean} => ({
+      fullScreen: !prevState.fullScreen,
+    }));
+  }
 
   getPlaces = () => {
-    if (!process.env.REACT_APP_API_HOST) throw new Error('REACT_APP_API_HOST missing')
-    axios.get(`${process.env.REACT_APP_API_HOST}/places`).then(({data}) => {
+    if (!process.env.REACT_APP_API_HOST) throw new Error('REACT_APP_API_HOST missing');
+    axios.get(`${process.env.REACT_APP_API_HOST}/places`).then(({ data }: {data: LocationsType}) => {
       this.setState({
-        locations: data
-      }) 
-    }).catch((error) => {
-      this.setState({error: true})
-    })
+        locations: data,
+      });
+    }).catch(() => {
+      this.setState({ error: true });
+    });
   }
 
   deletePlace = (itemID: number) => {
-    if (!process.env.REACT_APP_API_HOST) throw new Error('REACT_APP_API_HOST missing')
-    const URL = `${process.env.REACT_APP_API_HOST}/places/${itemID}`
+    if (!process.env.REACT_APP_API_HOST) throw new Error('REACT_APP_API_HOST missing');
+    const URL = `${process.env.REACT_APP_API_HOST}/places/${itemID}`;
     axios.delete(URL).then(() => {
-      const arr = this.state.locations
-      const findIndex = arr.findIndex((element) => element.id === itemID)
-      const result = findIndex >= 0 ? [...arr.slice(0, findIndex), ...arr.slice(findIndex + 1)] : arr
-      this.setState({locations: result, error: false})
-    }).catch((error) => {
-      this.setState({error: true})
-    })
+      const { locations } = this.state;
+      const func = (element: LocationType): boolean => element.id === itemID;
+      const findIndex = locations.findIndex(func);
+      const newArray = [...locations.slice(0, findIndex), ...locations.slice(findIndex + 1)];
+      const result = findIndex >= 0 ? newArray : locations;
+      this.setState({ locations: result, error: false });
+    }).catch(() => {
+      this.setState({ error: true });
+    });
   }
 
-  showError = () =>
-    this.setState({error: true})
+  handleKeyDown = (ev: KeyboardEvent) => {
+    if (ev.keyCode === 88) {
+      this.closeError();
+    }
+  }
 
-  renderError = () =>
+  showError = (): void => this.setState({ error: true })
+
+  closeError = (): void => this.setState({ error: false })
+
+  renderError = (): React$Node => (
     <div className="alert">
-      <span className="closebtn" onClick={() => this.setState({ error: false })}>&times;</span> 
-      <p style={{margin: 0}}>Something Went Wrong!</p>
+      <span className="closebtn" onClick={this.closeError} onKeyDown={this.handleKeyDown} tabIndex="0" role="button">
+      &times;
+      </span>
+      <p style={{ margin: 0 }}>Something Went Wrong!</p>
     </div>
+  )
 
-  addLocation = (location: Location) => {
-    this.setState({locations: [...this.state.locations, location]})
+  addLocation = (location: LocationType) => {
+    const { locations } = this.state;
+    this.setState({ locations: [...locations, location] });
   }
 
-  render() {
+  render(): React$Node {
+    const { fullScreen, error, locations } = this.state;
     return (
       <div>
-        { !this.state.fullScreen &&
-          <div className="header">
-            <a href="#default" className="logo">React Geocoding Exercise</a>
-          </div>
+        { !fullScreen
+          && (
+            <div className="header">
+              <a href="#default" className="logo">React Geocoding Exercise</a>
+            </div>
+          )
         }
-        {this.state.error && this.renderError()}
-        <div className='container'>
-          <GoogleMap data={this.state.locations} showError={this.showError} />
-          {
-            !this.state.fullScreen &&
-            <Search locations={this.state.locations} addLocation={this.addLocation} showError={this.showError} deletePlace={this.deletePlace} />
+        {error && this.renderError()}
+        <div className="container">
+          <GoogleMap data={locations} showError={this.showError} />
+          { !fullScreen
+              && (
+              <Search
+                locations={locations}
+                addLocation={this.addLocation}
+                showError={this.showError}
+                deletePlace={this.deletePlace}
+              />
+              )
           }
         </div>
       </div>
-    )
+    );
   }
 }
 
