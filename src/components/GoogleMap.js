f5f0f5a1741376cda01aa84d95ebff4150ac84e3
Sajad Rastegar <rastegar.sajjad@gmail.com>
2019-06-03 23:05:56 +0430

    Lint GoogleMap component

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,193 +1,206 @@
 // @flow
-import React, { PureComponent } from 'react'
-import GoogleMapReact from 'google-map-react'
-import '../styles/GoogleMap.css'
-import axios from 'axios'
-import Marker from './Marker'
+import React, { PureComponent } from 'react';
+import GoogleMapReact from 'google-map-react';
+import '../styles/GoogleMap.css';
+import axios from 'axios';
+import Marker from './Marker';
 
 const mapStyles = {
   width: '100%',
-  height: '100%'
-}
+  height: '100%',
+};
 
-const defaultCenter = { lat: 5.6219868, lng: -0.23223 }
+const defaultCenter = { lat: 5.6219868, lng: -0.23223 };
 
-type Location = {
+type LocationType = {
   id: number,
   location: string,
   lat: string,
   lon: string
 };
 
-type Locations = $ReadOnlyArray<Location>;
+type LocationsType = $ReadOnlyArray<LocationType>;
 
-type Props = {
-  data: Locations,
-  showError: () => void,
+type PropsType = {
+  data: LocationsType,
+  showError: () => void
 };
 
-type State = {
-  locations: Locations,
+type StateType = {
   center: { lat: number, lng: number },
-  activeMarker: ?Location,
+  activeMarker: ?LocationType,
   loading: boolean,
   draggable: boolean
 };
 
-type Coordinates = {
+type CoordinatesType = {
   lat: string,
   lng: string
 };
 
-class GoogleMap extends PureComponent<Props, State> {
-  _isMounted = false
-  _map: { current: null | HTMLDivElement }
+class GoogleMap extends PureComponent<PropsType, StateType> {
+  mounted = false
 
-  constructor(props: Props) {
-    super(props)
-    this.state = { center: defaultCenter, locations: this.props.data, activeMarker: null, loading: false, draggable: true }
-    this._map = React.createRef();
+  map: { current: null | HTMLDivElement }
+
+  constructor(props: PropsType) {
+    super(props);
+    this.state = {
+      center: defaultCenter, activeMarker: null, loading: false, draggable: true,
+    };
+    this.map = React.createRef();
   }
 
   componentDidMount() {
-    this._isMounted = true
+    this.mounted = true;
   }
 
   componentWillUnmount() {
-    this._isMounted = false
+    this.mounted = false;
   }
 
-  componentDidUpdate(prevProps: { data: Locations }) {
-    if(prevProps.data !== this.props.data) {
-      this.setState({locations: this.props.data})
-    }
+  markerClick = (item: LocationType) => {
+    this.setState({ activeMarker: item, center: { lat: Number(item.lat), lng: Number(item.lon) } });
   }
 
-  markerClick = (item: Location) => this.setState({activeMarker: item, center: {lat: Number(item.lat), lng: Number(item.lon)}})
-
-  markerTitle = (title: string) => {
+  markerTitle = (title: string): string => {
     if (title) {
-      if (title.length > 20) return title.substring(0, 20) + ' ...'
-      return title
+      if (title.length > 20) return `${title.substring(0, 20)} ...`;
+      return title;
     }
+    return 'Unknown Location';
   }
 
-  renderMarker = (item: Location) => {
+  renderMarker = (item: LocationType): React$Node => {
+    const { activeMarker } = this.state;
     return (
       <Marker
         key={item.id}
         data={item}
-        title={this.markerTitle(item.location) || 'Unknown Location'}
+        title={this.markerTitle(item.location)}
         lat={item.lat}
         lng={item.lon}
-        dataTestID={this.state.activeMarker ? "active-marker" : "marker"}
-        onClick={this.markerClick}>
-      </Marker>
-    )
+        dataTestID={activeMarker ? 'active-marker' : 'marker'}
+        onClick={this.markerClick}
+      />
+    );
   }
 
-  updateActiveMarkerLocation = ({lat, lng} : {lat : string, lng : string}) => {
+  updateActiveMarkerLocation = ({ lat, lng }: {lat: string, lng: string}) => {
+    const { activeMarker } = this.state;
     this.setState({
-      activeMarker: { ...this.state.activeMarker, lat: lat.toString(), lon: lng.toString()},
-      loading: true
-    })
+      activeMarker: { ...activeMarker, lat: lat.toString(), lon: lng.toString() },
+      loading: true,
+    });
   }
 
   reverseSearch = (lat: string, lon: string) => {
-    if (!process.env.REACT_APP_API_HOST) throw new Error('REACT_APP_API_HOST missing')
-    const URL = `${process.env.REACT_APP_API_HOST}/places/reverse_search`
-    axios.get(`${URL}/?lat=${lat}&lon=${lon}`).then(({data}) => {
+    const { activeMarker } = this.state;
+    const { showError } = this.props;
+    if (!process.env.REACT_APP_API_HOST) throw new Error('REACT_APP_API_HOST missing');
+    const URL = `${process.env.REACT_APP_API_HOST}/places/reverse_search`;
+    axios.get(`${URL}/?lat=${lat}&lon=${lon}`).then(({ data }: {data: Array<{data: { display_name: string }}>}) => {
       this.setState({
-        activeMarker: { ...this.state.activeMarker, location: data[0].data.display_name },
+        activeMarker: { ...activeMarker, location: data[0].data.display_name },
         loading: false,
         draggable: true,
-        center: { lat: Number(lat), lng: Number(lon)}
-      })
-    }).catch((error) => {
-      this.props.showError()
-    })
+        center: { lat: Number(lat), lng: Number(lon) },
+      });
+    }).catch(() => {
+      showError();
+    });
   }
 
-  renderMarkers = () => {
-    if (this.state.activeMarker) {
-      return this.renderMarker(this.state.activeMarker)
+  renderMarkers = (): Array<React$Node> => {
+    const { data } = this.props;
+    const { activeMarker } = this.state;
+    if (activeMarker) {
+      return [this.renderMarker(activeMarker)];
     }
-    if (this.state.locations) {
-      return this.state.locations.map<{}>(location => this.renderMarker(location))
-    } 
+    if (data) {
+      return data.map((location: LocationType): React$Node => this.renderMarker(location));
+    }
+    return [];
   }
 
   saveEdit = () => {
-    if (this.state.activeMarker && !this.state.loading) {
-      const {location, lat, lon, id} = this.state.activeMarker
-      if (!process.env.REACT_APP_API_HOST) throw new Error('REACT_APP_API_HOST missing')
-      const URL = `${process.env.REACT_APP_API_HOST}/places`
+    const { activeMarker, loading } = this.state;
+    const { showError } = this.props;
+    if (activeMarker && !loading) {
+      const {
+        location, lat, lon, id,
+      } = activeMarker;
+      if (!process.env.REACT_APP_API_HOST) throw new Error('REACT_APP_API_HOST missing');
+      const URL = `${process.env.REACT_APP_API_HOST}/places`;
       axios.patch(`${URL}/${id}`, {
         place: {
           location,
           lat,
-          lon
+          lon,
+        },
+      }).then(() => {
+        if (this.mounted) {
+          this.setState({ activeMarker: null });
         }
-      }).then(({data}) => {
-        const locations = this.state.locations.filter(el => el.id !== id)
-        
-        if (this._isMounted) {
-          this.setState({locations: [...locations, data], activeMarker: null})
-        }
-        
-      }).catch((error) => {
-        this.props.showError()
-      })
-    } 
+      }).catch(() => {
+        showError();
+      });
+    }
   }
 
-  cancelEdit = () =>
-    this.setState({activeMarker: null, center: defaultCenter})
+  cancelEdit = () => {
+    this.setState({ activeMarker: null, center: defaultCenter });
+  }
 
-  onChildMouseDown = (childKey: number, childProps: {}, mouse: Coordinates) => {
+  onChildMouseDown = () => {
     this.setState({
       draggable: false,
     });
   }
 
-  onChildMouseUp = (childKey: number, childProps: {}, mouse: Coordinates) => {
-    if (this.state.activeMarker) {
-      this.reverseSearch(mouse.lat, mouse.lng)
+  onChildMouseUp = (childKey: number, childProps: {}, mouse: CoordinatesType) => {
+    const { activeMarker } = this.state;
+    if (activeMarker) {
+      this.reverseSearch(mouse.lat, mouse.lng);
     }
   }
 
-  onChildMouseMove = (childKey: number, childProps: {}, mouse: Coordinates) => {
-    if (this.state.activeMarker) {
-      this.updateActiveMarkerLocation({lat: mouse.lat, lng: mouse.lng})
+  onChildMouseMove = (childKey: number, childProps: {}, mouse: CoordinatesType) => {
+    const { activeMarker } = this.state;
+    if (activeMarker) {
+      this.updateActiveMarkerLocation({ lat: mouse.lat, lng: mouse.lng });
     }
   }
 
-  render() {
+  render(): React$Node {
+    const { activeMarker, draggable, center } = this.state;
     return (
       <React.Fragment>
-        { this.state.activeMarker &&
+        { activeMarker
+          && (
           <div className="editBar">
-            <button className="button" onClick={this.saveEdit}>Save</button>
-            <button className="button" onClick={this.cancelEdit}>Cancel</button>
+            <button type="submit" className="button" onClick={this.saveEdit}>Save</button>
+            <button type="button" className="button" onClick={this.cancelEdit}>Cancel</button>
           </div>
+          )
         }
-        <div data-testid='google-map-container' className='map-container'>
+        <div data-testid="google-map-container" className="map-container">
           <GoogleMapReact
-            draggable={this.state.draggable}
+            draggable={draggable}
             style={mapStyles}
             bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
-            center={this.state.center}
+            center={center}
             zoom={1}
             onChildMouseDown={this.onChildMouseDown}
             onChildMouseUp={this.onChildMouseUp}
-            onChildMouseMove={this.onChildMouseMove}    
-            ref={this._map}
+            onChildMouseMove={this.onChildMouseMove}
+            ref={this.map}
           >
             {this.renderMarkers()}
           </GoogleMapReact>
         </div>
       </React.Fragment>
-    )
+    );
   }
 }
 
